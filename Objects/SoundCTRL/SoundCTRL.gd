91ca32ae8fe8e767707fe07483acd4fb10f97b27
Bryan Miller <bmiller1008@gmail.com>
2022-03-23 17:56:43 -0400

    Added some footstep sounds. Created a very crude soundCTRL node for playing SFX. TestDummy now has footsteps!

---
+++ Objects/SoundCTRL/SoundCTRL.gd
@@ -0,0 +1,105 @@
+extends Node2D
+
+# -------------------------------------------------------------------------
+# Export Variables
+# -------------------------------------------------------------------------
+export (Array, Array, String) var sounds = []
+export (Array, Array, String) var sound_sets = []
+
+# -------------------------------------------------------------------------
+# Variables
+# -------------------------------------------------------------------------
+var _sounds : Dictionary = {}
+var _rng : RandomNumberGenerator = null
+
+# -------------------------------------------------------------------------
+# Onready Variables
+# -------------------------------------------------------------------------
+onready var sfx1 : AudioStreamPlayer2D = get_node("SFX1")
+onready var sfx2 : AudioStreamPlayer2D = get_node("SFX2")
+
+# -------------------------------------------------------------------------
+# Override Methods
+# -------------------------------------------------------------------------
+func _ready() -> void:
+	_rng = RandomNumberGenerator.new()
+	_rng.seed = randi() * 821345
+	for sound in sounds:
+		add_sound(sound[0], sound[1])
+
+# -------------------------------------------------------------------------
+# Private Methods
+# -------------------------------------------------------------------------
+func _GetSoundsInSet(set_name : String) -> Array:
+	var snds : Array = []
+	for sset in sound_sets:
+		if sset[0] == set_name:
+			var slist = sset[1].split(",")
+			for sound_name in slist:
+				sound_name = sound_name.lstrip(" \t\r\n").rstrip(" \t\r\n")
+				if sound_name in _sounds:
+					snds.append(sound_name)
+	return snds
+
+# -------------------------------------------------------------------------
+# Public Methods
+# -------------------------------------------------------------------------
+func add_sound(sound_name : String, path : String) -> void:
+	if not sound_name in _sounds:
+		var dir : Directory = Directory.new()
+		if dir.file_exists(path):
+			var snd = load(path)
+			if snd and snd is AudioStream:
+				_sounds[sound_name] = snd
+			else:
+				printerr("WARNING: Resource file \"", path, "\" is not an AudioStream.")
+		else:
+			printerr("WARNING: Unable to find resource \"", path, "\".")
+	else:
+		printerr("WARNING: Sound name \"", sound_name, "\" already defined.")
+
+func play(sound_name : String) -> void:
+	if sound_name in _sounds:
+		var stream = _sounds[sound_name]
+		if sfx1.stream == stream or sfx2.stream == stream:
+			if sfx1.stream == stream:
+				if not sfx1.playing:
+					sfx1.play()
+			if sfx2.stream == stream:
+				if not sfx2.playing:
+					sfx2.play()
+		elif not sfx1.playing:
+			sfx1.stream = stream
+			sfx1.play()
+		elif not sfx2.playing:
+			sfx2.stream = stream
+			sfx2.play()
+
+func play_random_set(set_name : String) -> void:
+	var sound_list : Array = _GetSoundsInSet(set_name)
+	if sound_list.size() > 0:
+		var idx = _rng.randi_range(0, sound_list.size() - 1)
+		play(sound_list[idx])
+
+func is_playing() -> bool:
+	return sfx1.playing or sfx2.playing
+
+func is_playing_sound(sound_name : String) -> bool:
+	if sound_name in _sounds:
+		var stream = _sounds[sound_name]
+		if sfx1.stream == stream and sfx1.playing:
+			return true
+		if sfx2.stream == stream and sfx2.playing:
+			return true
+	return false
+
+func stop() -> void:
+	sfx1.stop()
+	sfx2.stop()
+
+
+
+# -------------------------------------------------------------------------
+# Handler Methods
+# -------------------------------------------------------------------------
+
